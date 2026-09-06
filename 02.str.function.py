feac530fdb062ed531db4df80f0eadb2f7e8e0bd
Sahil Gouri <sahilgouri098kh@gmail.com>
2026-09-06 13:22:59 +0530

    Add files via upload

---
+++ 02.str.function.py
@@ -0,0 +1,24 @@
+# name="sahil"
+# print(len(name))
+# print(name.endswith("il"))
+# print(name.startswith("s"))
+# "hello".lower()
+# "Hello".lower()  # "hello"
+# "hello".upper()  # "HELLO"
+
+#
+# s = "hello sahil. hi sahil"
+# capital=s.capitalize()
+# print(capital)
+
+# s= "hello sahil my name is nothing"
+# index=s.find("sahil") # output is 6 because sahil is at index 6
+# print(index)
+
+# s="hello sahil my name is sahil and i am nothing"      # in this code we repace the word
+# replace=s.replace("sahil","python")
+# print(replace)
+
+food = "pizza"
+print(food.replace("z","s"))
+print(food)
